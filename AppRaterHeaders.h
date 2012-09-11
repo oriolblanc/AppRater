aa1a36a5d81cc59e81fe7d0bdb394361b9f0e336
Oriol Blanc <oriolblanc@gmail.com>
2012-09-11 15:31:36 +0200

    Headers

--- AppRaterHeaders.h
+++ AppRaterHeaders.h
@@ -9,8 +9,19 @@
 
 @protocol AppRater <NSObject>
     @property (nonatomic, assign) BOOL debug;
+    @property (nonatomic, assign) int usesToPrompt;
+    @property (nonatomic, assign) float daysToPrompt;
+    @property (nonatomic, assign) float daysToRemind;
+
+    @property (nonatomic, copy) NSString *alertTitle;
+    @property (nonatomic, copy) NSString *alertMessage;
+    @property (nonatomic, copy) NSString *alertCancelButton;
+    @property (nonatomic, copy) NSString *alertRemindButton;
+    @property (nonatomic, copy) NSString *alertRateButton;
     + (AppRater *)instance;
+    + (void)rate;
 
 // setters
     + (void)setDebug:(BOOL)debug;
 @end
+
