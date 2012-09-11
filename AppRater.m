480064c178caecb7128099876363515f7454ea87
Oriol Blanc <oriolblanc@gmail.com>
2012-09-11 15:29:50 +0200

    Initializing AppRater

--- AppRater.m
+++ AppRater.m
@@ -8,5 +8,41 @@
 #import "AppRater.h"
 
 @implementation AppRater
+@synthesize debug = _debug;
+
++ (void)takeOff:(NSString *)appId
+{
+    [[self instance] setUpAppRater];
+}
+
++ (AppRater *)instance 
+{
+	static AppRater *appRater = nil;
+	if (appRater == nil)
+	{
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            appRater = [[AppRater alloc] init];
+        });
+	}
+    
+	return appRater;
+}
+
+- (id)init
+{
+    if ((self = [super init]))
+    {        
+        self.debug = NO;
+        
+        // On iOS 4.0+ only, listen for foreground notification
+        if(&UIApplicationWillEnterForegroundNotification != nil)
+        {
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setUpAppRater) name:UIApplicationWillEnterForegroundNotification object:nil];
+        }
+    }
+    return self;
+}
+
 
 @end
