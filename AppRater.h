8d5aa1cd89d4bbfaf9be65ed566ef5643a58ce88
Oriol Blanc <oriolblanc@gmail.com>
2012-09-11 13:40:37 +0200

    Added AppRater

---
+++ AppRater.h
@@ -0,0 +1,10 @@
+//
+//  AppRater.m
+//
+//  Created by Oriol Blanc Gimeno on 25/07/12.
+//  Copyright (c) 2012 Oriol Blanc Gimeno. All rights reserved.
+//
+
+@interface AppRater : NSObject
+
+@end
---
+++ AppRater.m
@@ -0,0 +1,12 @@
+//
+//  AppRater.m
+//
+//  Created by Oriol Blanc Gimeno on 25/07/12.
+//  Copyright (c) 2012 Oriol Blanc Gimeno. All rights reserved.
+//
+
+#import "AppRater.h"
+
+@implementation AppRater
+
+@end
